7385583cc01a49d189efbd653392f97da4a12a21
Author Name <Author Email>
2018-11-02 20:34:21 +0800

    'finsh'

--- src/javascript/controllers/login.js
+++ src/javascript/controllers/login.js
@@ -1,7 +1,7 @@
 import login_box from '../views/login-box.html'
 import qs from 'querystring'
 import login_model from '../models/login'
-
+import toast from '../utils/toast'
 
 const init = ()=>{
     render() // 渲染视图
@@ -25,12 +25,13 @@ const bindEvent = ()=>{
         e.preventDefault()
         let _params = $(this).serialize()
         let _result = await login_model.signup(qs.parse(_params))
-        console.log(123)
         switch ( _result.status ) {
-            case 500: alert('失败，服务器出了问题'); break;
-            case 201: alert('用户已存在'); break;
+            case 500: toast('失败，服务器出了问题'); break;
+            case 201: toast('用户已存在'); break;
             default: 
-                alert('注册成功');
+                toast('注册成功');
+                $('.signup_body').css('display','none')
+                $('.signin_body').css('display','block')
                 break;
         }
     })
@@ -38,16 +39,13 @@ const bindEvent = ()=>{
     $('.signin_body').on('submit', '#signin_form', async function (e) {
         e.preventDefault()
         let _params = $(this).serialize()
-        $.cookie('connect.sid', { expires: -1 })
         let _result = await login_model.signin(qs.parse(_params))
-        console.log(123)
         switch ( _result.status ) {
-            case 203: alert('密码错误'); break;
-            case 202: alert('用户不存在'); break;
+            case 203: toast('密码错误'); break;
+            case 202: toast('用户不存在'); break;
             default: 
-                window.location.href = "/"; 
-                console.log(123)
-                alert(123)
+                window.location.href = "/";
+                toast('登录成功')
                 break;
         }
     })
--- src/javascript/login.js
+++ src/javascript/login.js
@@ -1,4 +1,4 @@
-console.log("hello 盖亚")
+
 import '../css/login.scss'
 import login_controllers from './controllers/login'
 
